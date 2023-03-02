6397f472f96d2e448f7c6180a132a4c6538b7d25
Pushti Soalnki <87349225+pushtii26@users.noreply.github.com>
2023-03-03 00:24:34 +0530

    Update Greet.js

--- tasks/week1/src/components/Greet.js
+++ tasks/week1/src/components/Greet.js
@@ -1,1 +1,18 @@
+import React from 'react'
 
+// function Greet() {
+//   return <h1>Hello Pushti</h1>
+// }
+
+const Greet = props => {
+  return (
+    <>
+    <h2>
+        {props.name} {props.heroName}
+      </h2>
+      <p>{props.children}</p>
+    </>
+  )
+}
+
+export default Greet;
